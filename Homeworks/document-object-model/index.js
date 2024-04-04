7f1212087871f3190cc457b152c6982b5ba79981
Tsiniavska <yulia.tsiniavska@com>
2024-04-04 17:58:21 +0300

    homework DOM

--- Homeworks/document-object-model/index.js
+++ Homeworks/document-object-model/index.js
@@ -1,45 +1,64 @@
 // get element playlist
 const DOMplaylist = document.getElementById('js-playlist');
-// console.log(DOMplaylist);
+console.log(DOMplaylist);
 
 if (DOMplaylist) {
-  const addTitle = (text) => {
-    const h3 = document.createElement('h3');
-    h3.classList.add('playlist-title');
-    h3.innerText = 'Playlist';
-    DOMplaylist.appendChild(h3);
-    // create ol
-    const ol = document.createElement('ol');
-    DOMplaylist.appendChild(ol);
-    return ol;
-  };
-  // add li to ol
-  const addItemToList = (text) => {
-    const ol = DOMplaylist.querySelector('ol');
+  const addItemToList = (item) => {
     const li = document.createElement('li');
     li.classList.add('list-item');
-    ol.appendChild(li);
-    return li;
-  };
-  const addTextList = (texts) => {
-    const li = addItemToList();
-    texts = texts.split(', ');
-    for (let i = 0; i < 2; i++) {
-      const div = document.createElement('div');
-      const span = document.createElement('span');
-      span.innerText = texts[i];
-      div.appendChild(span);
-      li.appendChild(div);
-    }
+    const authorSpan = document.createElement('span');
+    authorSpan.classList.add('item');
+    authorSpan.innerText = `author: ${item.author}`;
+    li.appendChild(authorSpan);
+
+    const songSpan = document.createElement('span');
+    songSpan.classList.add('item');
+    songSpan.innerText = `song: ${item.song}`;
+    li.appendChild(songSpan);
+    DOMplaylist.appendChild(li);
   };
 
-  addTitle('Playlist');
-  addTextList('author: "LED ZEPPELIN",, song:"STAIRWAY TO HEAVEN"');
-  addTextList('author: "QUEEN",, song:"BOHEMIAN RHAPSODY"');
-  addTextList('author: "LYNYRD SKYNYRD",, song:"FREE BIRD"');
-  addTextList('author: "DEEP PURPLE",, song:"SMOKE ON THE WATER"');
-  addTextList('author: "JIMI HENDRIX",, song:"ALL ALONG THE WATCHTOWER"');
-  addTextList('author: "AC/DC",, song:"BACK IN BLACK"');
-  addTextList('author: "QUEEN",, song:"WE WILL ROCK YOU"');
-  addTextList('author: "METALLICA",, song:"ENTER SANDMAN"');
+  [
+    {
+      author: 'LED ZEPPELIN',
+      song: 'STAIRWAY TO HEAVEN',
+    },
+
+    {
+      author: 'QUEEN',
+      song: 'BOHEMIAN RHAPSODY',
+    },
+
+    {
+      author: 'LYNYRD SKYNYRD',
+      song: 'FREE BIRD',
+    },
+
+    {
+      author: 'DEEP PURPLE',
+      song: 'SMOKE ON THE WATER',
+    },
+
+    {
+      author: 'JIMI HENDRIX',
+      song: 'ALL ALONG THE WATCHTOWER',
+    },
+
+    {
+      author: 'AC/DC',
+      song: 'BACK IN BLACK',
+    },
+
+    {
+      author: 'QUEEN',
+      song: 'WE WILL ROCK YOU',
+    },
+
+    {
+      author: 'METALLICA',
+      song: 'ENTER SANDMAN',
+    },
+  ].forEach((item) => {
+    addItemToList(item);
+  });
 }
